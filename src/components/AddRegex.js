621d7d67117a8f58677ea23971fb85e98275feed
Aaron Llanos <aaron.lls.dev@gmail.com>
2021-04-10 05:14:08 -0500

    Version final

--- src/components/AddRegex.js
+++ src/components/AddRegex.js
@@ -1,11 +1,10 @@
 import React, {useState} from 'react'
-// import { getRegex } from '../helpers/getRegex'
-
 
 export const AddRegex = () => {
 
 	const [inputValue, setInputValue] = useState('')
 	const [regex, setRegex] = useState('')
+	const [showCopy, setShowCopy] = useState(false);
 
 	const handleInputChange = (e) => {
 		setInputValue(e.target.value)
@@ -14,6 +13,8 @@ export const AddRegex = () => {
 	const handleSubmit = (e) => {
 		e.preventDefault()
 		getRegex(inputValue)
+		setShowCopy(false)
+		setInputValue('')
 	}
 
 	const getRegex = (url) => {
@@ -35,20 +36,45 @@ export const AddRegex = () => {
 		setRegex(resultado)
 	}
 
+	const handleCopy = () => {
+		const textoRegex = document.querySelector('.texto-regex')
+		textoRegex.select()
+		document.execCommand('copy')
+		setShowCopy(true)
+	}
+
 	return (
 		<>
-			<form onSubmit={handleSubmit}>
-				<div className="search-contend">
-					<i className="fas fa-search"></i>
-					<input
-						type="text"
-						value={inputValue}
-						onChange={handleInputChange}
-					/>
-				</div>
-			</form>
-
-			<p>{regex}</p>
+			<div className="input-content">
+				<h2>Ingresa tu Regex</h2>
+				<form onSubmit={handleSubmit}>
+					<div className="search-content">
+						<input
+							className="input-search"
+							type="text"
+							value={inputValue}
+							onChange={handleInputChange}
+						/>
+					</div>
+				</form>
+			</div>
+			{
+				regex && (
+					<div className="regex-content animate__animated animate__fadeInRightBig">
+						<i class="fas fa-copy" onClick={handleCopy}></i>
+						<input className="texto-regex" type="text" value={regex}/>
+						<span id="#message"></span>
+					</div>
+				)
+			}
+			{
+				showCopy && (
+					<div className="text-copy-content animate__animated animate__fadeIn">
+						<p>Regex Copiado</p>
+						<i class="far fa-check-circle"></i>
+					</div>
+				)
+			}
 		</>
 	)
 }
